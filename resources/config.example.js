f331b0ab384d7db85225776e3e578e9adbabfa83
Ryan Mason <ryanmasonjar@gmail.com>
2019-12-11 20:51:38 -0500

    update example logs

--- resources/config.example.js
+++ resources/config.example.js
@@ -1,4 +1,8 @@
-// rename this file to config.js and leave the firebase_token and build as an empty strings
+/**
+ * Rename this file to config.js and leave the firebase_token, build,
+ * version as an empty strings. the app wont work without renaming this file
+ */
+
 export default {
   firebase_token: "",
   build: "",
