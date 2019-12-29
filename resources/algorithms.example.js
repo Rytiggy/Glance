d2eb6ab703b84de9069f0afa4bb62b6ef4ed66fa
Ryan Mason <ryanmasonjar@gmail.com>
2019-12-29 13:42:12 -0500

    Algorithms example file

---
+++ resources/algorithms.example.js
@@ -0,0 +1,9 @@
+// Calculate remaining IOB
+export let updateIOB = (entry, store) => {
+  return false;
+};
+
+// calculate remaining cob
+export let updateCOB = (entry, store) => {
+  return false;
+};
