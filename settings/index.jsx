b607d342f9244b30f1d6e06a3277a8a2d4d00c0a
Ben Doerr <craftsman@bendoerr.me>
2024-06-30 12:10:07 -0400

    Fixes syntax error in settings/index.jsx for CodeQL parsing

--- settings/index.jsx
+++ settings/index.jsx
@@ -280,7 +280,7 @@ function mySettings(props) {
 
         {props.settings.bgColor ? (
           JSON.parse(props.settings.bgColor) == "#FFFFFF" ? (
-            <Section title={}>
+            <Section>
               <Text bold align="center">
                 Random Color Generator
               </Text>
