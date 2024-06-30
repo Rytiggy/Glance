8ba0df129365821e19d57c667ad96b6f02577136
Ben Doerr <craftsman@bendoerr.me>
2024-06-30 14:57:31 -0400

    website: Update to vue 3 to resolve dependency security issues (#269)

--- website/src/boot/axios.js
+++ website/src/boot/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export default async ({ Vue }) => {
-  Vue.prototype.$axios = axios;
+export default async ({ app, Vue }) => {
+  app.$axios = axios
 };
--- website/src/router/routes.js
+++ website/src/router/routes.js
@@ -1,24 +1,23 @@
 const routes = [
   {
-    path: "/",
-    component: () => import("layouts/MyLayout.vue"),
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: "", component: () => import("pages/Index.vue") },
+      { path: '', component: () => import('pages/Index.vue') },
       {
         path: "/troubleshooting",
         component: () => import("pages/Troubleshooting.vue")
       },
       { path: "/customize", component: () => import("pages/Customize.vue") }
     ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue')
   }
-];
+]
 
-// Always leave this as last one
-if (process.env.MODE !== "ssr") {
-  routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
-  });
-}
-
-export default routes;
+export default routes
